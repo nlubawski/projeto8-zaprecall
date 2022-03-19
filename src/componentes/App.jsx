e9d95ef18afac7f82b041fbb857d9e6edeff46f4
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-03-18 22:42:53 -0300

    estilizacao da cor de fundo passado da TelaInicial para a main criada no App

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -2,10 +2,10 @@ import TelaInicial from "./TelaInicial"
 
 export default function App(){
     return (
-        <>
+        <main>
             <TelaInicial />
             
-        </>
+        </main>
         
     )
 }
